8528043b2a9427cf9bd25e57531f976edd7cdbf5
satyam <raj@gmail.com>
2022-02-09 15:17:45 +0530

    test completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 function myFunction(){
-    
-    
     var name=document.getElementById("name1").value;
     var exp=/^[a-zA-Z\s]+$/;
     var val = exp.test(name);
-
     document.getElementById("name1").style.border="1px solid black";
     document.getElementById("p1").innerText="";
     var mobile = document.getElementById("mobile1").value;
     document.getElementById("mobile1").style.border="1px solid black";
     document.getElementById("p2").innerText="";
     if(name == ""){
-        
         document.getElementById("name1").style.border= "1px solid red";
         document.getElementById("p1").innerText="this field is required!";
     }
@@ -21,11 +17,8 @@ function myFunction(){
         document.getElementById("p1").innerText="this field is required!";
 
     }
-  
     if(mobile.length != 10 ){
       document.getElementById("mobile1").style.border= "1px solid red";
       document.getElementById("p2").innerText="only 10 digits!";
     }
-
-
 }
